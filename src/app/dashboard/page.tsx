be4790f063fa8619914098a9df363f8c9441638e
opencpv <sulemanaadams375@gmail.com>
2023-09-11 14:09:14 +0000

    added dashboard and settings page starter

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Head>
+        <title>Settings - RentRightGh</title>
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Settings = () => {
+  return (
+    <>
+      <Head>
+        <title>Dashboard - RentRightGh</title>
+      </Head>
+      <main></main>
+    </>
+  );
+};
+
+export default Settings;
