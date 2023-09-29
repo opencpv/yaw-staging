861b5b96a679fef18dd71beae1393e2724190f10
opencpv <sulemanaadams375@gmail.com>
2023-09-29 23:41:18 +0000

    removed prime provider

--- src/context/NavMenuProvider.tsx
+++ src/context/NavMenuProvider.tsx
@@ -1,31 +1,35 @@
 "use client";
 import { createContext, useState } from "react";
-import {usePathname, useRouter} from "next/navigation"
+import { usePathname, useRouter } from "next/navigation";
 
 export const NavMenuContext = createContext({});
 
-
 const NavMenuProvider = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname()
-  const router = useRouter()
+  const pathname = usePathname();
+  const router = useRouter();
 
   const [isNavMenuShowing, setIsNavMenuShowing] = useState<boolean>(false);
 
   const openNavMenu = () => {
     setIsNavMenuShowing(true);
-    sessionStorage.setItem('currentRoute', pathname)
+    sessionStorage.setItem("currentRoute", pathname);
   };
   const closeNavMenu = () => {
     setIsNavMenuShowing(false);
-    const currentRoute = sessionStorage.getItem('currentRoute')
+    const currentRoute = sessionStorage.getItem("currentRoute");
     if (currentRoute) {
-      router.push(currentRoute)
+      router.push(currentRoute);
     }
   };
 
   return (
     <NavMenuContext.Provider
-      value={{ isNavMenuShowing, openNavMenu, closeNavMenu, setIsNavMenuShowing }}
+      value={{
+        isNavMenuShowing,
+        openNavMenu,
+        closeNavMenu,
+        setIsNavMenuShowing,
+      }}
     >
       {children}
     </NavMenuContext.Provider>
