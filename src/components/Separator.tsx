abfefb1fe18994e4a3188bd1a40614f47cf6ab98
opencpv <sulemanaadams375@gmail.com>
2023-09-29 20:15:13 +0000

    fixed seperator error

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import * as SeparatorInner from '@radix-ui/react-separator';
-import { styled } from '@stitches/react';
+import React from "react";
+import * as SeparatorInner from "@radix-ui/react-separator";
+import { styled } from "@stitches/react";
 
-type Props = {} & SeparatorInner.SeparatorProps;
+type Props = Omit<SeparatorInner.SeparatorProps, "color"> & {
+  color?: "primary" | "secondary" | "white";
+};
 const Separator = (props: Props) => <SeparatorRoot {...props} />;
 
 const SeparatorRoot = styled(SeparatorInner.Root, {
-  '&[data-orientation=horizontal]': { height: 1, width: '100%' },
-  '&[data-orientation=vertical]': { height: '100%', width: 1 },
+  "&[data-orientation=horizontal]": { height: 1, width: "100%" },
+  "&[data-orientation=vertical]": { height: "100%", width: 1 },
   variants: {
     color: {
       primary: {
-        backgroundColor: '#830009',
+        backgroundColor: "#830009",
       },
       secondary: {},
       white: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
       },
     },
   },
   defaultVariants: {
-    color: 'primary',
+    color: "primary",
   },
 });
 
