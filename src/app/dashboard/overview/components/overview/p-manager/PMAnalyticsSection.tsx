244ce6cfe23bb578711ec49d31490668fca33f74
Enoch Ansah <enocansah@yahoo.com>
2023-12-15 01:55:00 +0000

    Add: analytics on lister dashboard overview

--- src/app/dashboard/overview/components/overview/p-manager/PMAnalyticsSection.tsx
+++ src/app/dashboard/overview/components/overview/p-manager/PMAnalyticsSection.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
+import WhatsAppJoinCard from "./WhatsAppJoinCard";
+import PageVisitAnalytics from "./PageVisitAnalytics";
 
-type Props = {}
+type Props = {};
 
 const PMAnalyticsSection = (props: Props) => {
   return (
-    <div className='mb-20'>
-        WhatsApp and Analytics
+    <div className="grid-cols-2 gap-8 mb-20 space-y-8 md:grid md:space-y-0">
+      <WhatsAppJoinCard />
+      <PageVisitAnalytics />
     </div>
-  )
-}
+  );
+};
 
-export default PMAnalyticsSection
+export default PMAnalyticsSection;
