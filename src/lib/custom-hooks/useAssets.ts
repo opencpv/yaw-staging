5b617bb942f201cde3453e368e66c4dea047465c
opencpv <sulemanaadams375@gmail.com>
2023-12-19 13:58:12 +0000

    added icons

--- src/lib/custom-hooks/useAssets.ts
+++ src/lib/custom-hooks/useAssets.ts
@@ -34,13 +34,15 @@ import PeopleSell from "/public/assets/icons/overview/people-sell.svg";
 import Rafiki from "/public/assets/icons/overview/rafiki.svg";
 import ProfileImage from "/public/assets/images/profile-image.jpg";
 import HomeLanding from "/public/assets/images/home/landing.jpg";
-import BusinessPersonWithHouseKeys from "/public/assets/images/dashboard/businessman-house-keys.png"
-import LaptopDisplayingHome from "/public/assets/images/dashboard/laptop-displaying-homepage.png"
-import CoupleBlurred from "/public/assets/images/about/about-slider-img.webp"
-import PersonHoldingHouse from "/public/assets/images/dashboard/person-holding-house.jpeg"
-import _3DImage from "/public/assets/images/dashboard/overview/p-manager/3d-image.jpeg"
-import GetFeaturedImage from "/public/assets/images/dashboard/overview/p-manager/get-featured-image.jpeg"
-import VerifyListing from "/public/assets/images/dashboard/overview/p-manager/verify-listings-image.jpeg"
+import BusinessPersonWithHouseKeys from "/public/assets/images/dashboard/businessman-house-keys.png";
+import LaptopDisplayingHome from "/public/assets/images/dashboard/laptop-displaying-homepage.png";
+import CoupleBlurred from "/public/assets/images/about/about-slider-img.webp";
+import PersonHoldingHouse from "/public/assets/images/dashboard/person-holding-house.jpeg";
+import _3DImage from "/public/assets/images/dashboard/overview/p-manager/3d-image.jpeg";
+import GetFeaturedImage from "/public/assets/images/dashboard/overview/p-manager/get-featured-image.jpeg";
+import VerifyListing from "/public/assets/images/dashboard/overview/p-manager/verify-listings-image.jpeg";
+import SearchIcon from "/public/svgs/SearchHomeIcon.svg";
+import Clipboard from "/public/assets/images/dashboard/clipboard.png";
 
 export const useAssets = () => {
   return {
