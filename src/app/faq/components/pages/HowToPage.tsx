0f7f542e630f251c9e112517448057ce7ed049e0
Enoch Ansah <enocansah@yahoo.com>
2023-12-26 00:30:17 +0000

    Implement: Scrolling into appropriate view on hash change

--- src/app/faq/components/pages/HowToPage.tsx
+++ src/app/faq/components/pages/HowToPage.tsx
@@ -1,13 +1,17 @@
+"use client"
 import React from "react";
 import HowToVideosSection from "../how_to/HowToVideosSection";
 import OptionFilterTabs from "@/components/__shared/OptionFilterTabs";
 import { useHowToTabsStore } from "@/store/faq/useFaqStore";
+import { useHashChangeScroll } from "@/lib/custom-hooks/useWindowEvents";
 
 type Props = {};
 
 const HowToPage = (props: Props) => {
   const activeTab = useHowToTabsStore((state) => state.activeTab);
   const setActiveTab = useHowToTabsStore((state) => state.setActiveTab);
+
+  useHashChangeScroll()
 
   return (
     <section id="G9mELbIrnVmy7A==">
