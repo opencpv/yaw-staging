01f626e19645f60e08b11d292bc0be0e93e1b782
Enoch Ansah <enocansah@yahoo.com>
2024-01-09 16:47:05 +0000

    update: change image on general form to slider on contact

--- src/app/contact/components/ContactFormSideImage.tsx
+++ src/app/contact/components/ContactFormSideImage.tsx
@@ -1,3 +1,4 @@
+import SliderPaginationOnly from "@/components/__shared/sliders/SliderPaginationOnly";
 import { useAssets } from "@/lib/custom-hooks/useAssets";
 import { useContactStore } from "@/store/contact/useContactStore";
 import Image from "next/image";
@@ -17,14 +18,23 @@ const ContactFormSideImage = (props: Props) => {
   };
 
   return (
-    <div className="relative flex items-center flex-1 w-full aspect-square lg:mt-10 md:aspect-auto">
-      <Image
-        src={activeImages[active]}
-        alt="ad"
-        className="rounded-[8px] h-full max-h-[807px]"
-        fill
-        style={{ objectFit: "cover" }}
-        objectPosition="bottom"
+    // <div className="relative flex items-center flex-1 w-full aspect-square lg:mt-10 md:aspect-auto">
+    //   <Image
+    //     src={activeImages[active]}
+    //     alt="ad"
+    //     className="rounded-[8px] h-full max-h-[807px]"
+    //     fill
+    //     style={{ objectFit: "cover" }}
+    //     objectPosition="bottom"
+    //   />
+    // </div>
+    <div className="flex-1 md:mt-10">
+      <SliderPaginationOnly
+        images={[1, 2, 3].map((image) => ({
+          src: "/assets/images/niceHome.png",
+          name: "",
+        }))}
+        className="aspect-square w-full md:h-[40rem] md:w-full"
       />
     </div>
   );
