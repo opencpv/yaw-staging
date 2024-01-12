45545f7a55d51e69603075e79fb22b7a0a896ef9
Enoch Ansah <enocansah@yahoo.com>
2024-01-12 15:39:05 +0000

    update: phone number input

--- src/components/__shared/form/InputPhoneNumber.tsx
+++ src/components/__shared/form/InputPhoneNumber.tsx
@@ -1,7 +1,9 @@
 "use client";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
-import React, { useRef, useState } from "react";
+import React, {
+  useState,
+} from "react";
 import { E164Number, CountryCode } from "libphonenumber-js/core";
 
 type Props = {
@@ -23,15 +25,19 @@ const InputPhoneNumber: React.FC<Props & React.HTMLProps<HTMLInputElement>> = ({
   onCountryChange,
   placeholder,
 }) => {
+
   const [country] = useState<CountryCode>("GH");
-  const [showCode, setShowCode] = useState<boolean>(false);
+  const [showCode, setShowCode] = useState<boolean>(false)
 
-  const inputRef = useRef(null);
+  document.querySelector(".PhoneInput")?.addEventListener("focus", () => {
+      console.log("FOCUS")
+  })
 
-  const handleFocus = () => {
-    // setShowCode(true);
-    setShowCode(true); // intentionally called thrice as once doesn't work as intended
-  };
+  const handleFocus = (e: any) => {
+      setShowCode(true)
+      setShowCode(true)
+      setShowCode(true) // intentionally called thrice as once doesn't work as intended
+  }
 
   return (
     <PhoneInput
@@ -46,7 +52,6 @@ const InputPhoneNumber: React.FC<Props & React.HTMLProps<HTMLInputElement>> = ({
       placeholder={placeholder}
       onFocus={handleFocus}
       onBlur={onBlur}
-      ref={inputRef}
     />
   );
 };
