c73a1270500ac835a1ad4c4513f34896a93499d6
Enoch Ansah <enocansah@yahoo.com>
2024-01-16 02:31:11 +0000

    update: ScrollTop button

--- src/components/__shared/ScrollTop.tsx
+++ src/components/__shared/ScrollTop.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import { FaChevronUp } from "react-icons/fa";
 import { useMenuStore } from "@/store/navmenu/useMenuStore";
 
-
 type Props = {
   scrollTo: string;
 };
@@ -13,7 +12,7 @@ const ScrollTop = () => {
   const [shouldShowScrollBtn, setShouldShowScrollBtn] =
     useState<boolean>(false);
   const [isScrollingUp, setIsScrollingUp] = useState<boolean>(true);
-  const isMenuOpen = useMenuStore((state) => state.toggle)
+  const isMenuOpen = useMenuStore((state) => state.toggle);
 
   const scrollUpwards = () => {
     window.scroll({
@@ -59,12 +58,12 @@ const ScrollTop = () => {
 
   return (
     <div
-      className={`grid place-items-center fixed right-1 z-50 top-[90%] h-10 w-10 shadow-lg bg-gradient-to-t from-[#073B3A] to-primary-400 cursor-pointer rounded-full sm:h-14 sm:w-14  ${
+      className={`fixed right-1 top-[90%] z-50 grid h-10 w-10 cursor-pointer place-items-center rounded-full bg-gradient-to-t from-[#073B3A] to-primary-400 shadow-lg sm:h-14 sm:w-14  ${
         isScrollingUp && shouldShowScrollBtn ? "opacity-70" : "opacity-0"
-      } ${isMenuOpen && "hidden" } transition-opacity sm:right-10`}
+      } ${isMenuOpen && "hidden"} transition-opacity sm:right-10`}
       onClick={scrollUpwards}
     >
-      <FaChevronUp className="text-white text-2xl font-[900]" />
+      <FaChevronUp className="text-2xl font-[900] text-white" />
     </div>
   );
 };
