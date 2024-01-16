163b44dc501636594c86920150b35ef5c018200a
Enoch Ansah <enocansah@yahoo.com>
2024-01-16 13:00:39 +0000

    Fix: scrollTop button showing on menu open

--- src/app/components/slider/promotion/PromotionSlider.tsx
+++ src/app/components/slider/promotion/PromotionSlider.tsx
@@ -26,18 +26,17 @@ const PromotionSlider = () => {
       <Swiper
         direction={"vertical"}
         allowTouchMove={false}
-        
         pagination={{
           clickable: true,
           el: ".custom-pagination-ver",
         }}
         modules={[Pagination, Autoplay]}
-        className={`mySwiper relative rounded-3xl w-full h-[29rem]`}
+        className={`mySwiper relative h-[29rem] w-full rounded-3xl`}
       >
         {/* Promotee info from database */}
         {images.map((image, idx) => (
           <SwiperSlide key={idx + 1}>
-            <div className="relative w-full h-full">
+            <div className="relative h-full w-full">
               <Image
                 src={image}
                 alt=""
@@ -47,30 +46,30 @@ const PromotionSlider = () => {
               />
             </div>
             {/* Promotion label */}
-            <div className="absolute z-50 top-32 left-3 space-y-20 min-[300px]:left-10">
+            <div className="absolute left-3 top-32 z-50 space-y-20 min-[300px]:left-10">
               <div className="space-y-3 text-sm ">
-                <h1 className="text-accent-100 font-[700] text-3xl">
+                <h1 className="text-3xl font-[700] text-accent-100">
                   Promotion
                 </h1>
                 <p className="text-white">Lorem ipsum</p>
                 <Link href="" className="inline-block">
-                  <Button className="flex items-center gap-3 text-white capitalize border-none rounded-md min-w-fit bg-accent-200 hover:bg-neutral-300 hover:text-neutral-600">
+                  <Button className="flex min-w-fit items-center gap-3 rounded-md border-none bg-accent-200 capitalize text-white hover:bg-neutral-300 hover:text-neutral-600">
                     View item <IoIosArrowRoundForward />
                   </Button>
                 </Link>
               </div>
               {/* Promotion description */}
-                <p className="relative z-30 text-white text-sm font-[600] leading-relaxed w-10/12 line-clamp-5 lg:ml-10">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Exercitationem nulla possimus, eum amet minima dicta ducimus
-                  facilis? Libero, deserunt quam molestias tempore, assumenda
-                  modi mollitia pariatur et quas, exercitationem tenetur?
-                </p>
+              <p className="relative z-30 line-clamp-5 w-10/12 text-sm font-[600] leading-relaxed text-white lg:ml-10">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Exercitationem nulla possimus, eum amet minima dicta ducimus
+                facilis? Libero, deserunt quam molestias tempore, assumenda modi
+                mollitia pariatur et quas, exercitationem tenetur?
+              </p>
             </div>
           </SwiperSlide>
         ))}
-        <div className="absolute z-10 bottom-0 left-0 p-5 w-full bg-primary-500 bg-opacity-25 min-h-[10rem] xs:p-10">
-          <div className="absolute mr-3 -mt-10 custom-pagination-ver md:mr-20"></div>
+        <div className="absolute bottom-0 left-0 z-10 min-h-[10rem] w-full bg-primary-500 bg-opacity-25 p-5 xs:p-10">
+          <div className="custom-pagination-ver absolute -mt-10 mr-3 md:mr-20"></div>
         </div>
       </Swiper>
     </>
