ce62a6641e2de8e60d8f35e2d382e520e7b6eba1
Enoch Ansah <enocansah@yahoo.com>
2024-01-16 23:30:22 +0000

    Update: login button font size and gap

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,22 +12,19 @@ const Login = () => {
       <Head>
         <title>Login - RentRightGh</title>
       </Head>
-      <main className="grid w-full min-h-screen grid-cols-1 bg-darkGreenBg lg:grid-cols-2">
+      <main className="grid w-full h-[100svh] max-h-[100svh] overflow-y-auto hidden-scrollbar grid-cols-1 bg-darkGreenBg lg:grid-cols-2">
         <div className="hidden w-full min-h-screen lg:block lg:relative">
           <Image
             src={images.StockImage}
-            alt="bg"
+            alt="room"
             fill
             style={{ objectFit: "cover", objectPosition: "center" }}
           />
         </div>
-        <div className="relative flex flex-col items-center justify-between w-full h-full py-5 bg-plane-pattern px-5 lg:py-8">
+        <div className="relative flex flex-col gap-10 items-center justify-between w-full h-full py-5 bg-plane-pattern px-5 lg:py-8">
           <LoginForm />
           <LoginFooter />
         </div>
-        {/* <div className="w-full col-span-2 ">
-          <Footer />
-        </div> */}
       </main>
     </>
   );
