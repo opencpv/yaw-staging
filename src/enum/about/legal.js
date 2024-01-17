2893000de293654eaf5a1267637a0f1721b15ae3
Enoch Ansah <enocansah@yahoo.com>
2024-01-17 00:04:22 +0000

    Add: legal details (telephone, copyright, email, etc)as one source of truth

---
+++ src/enum/about/legal.js
@@ -0,0 +1,9 @@
+const legal = {
+  copyrightNotice: "All rights reserved",
+  companyName: "ESODO LLC",
+  email: "contact@rightrightGH.com",
+  telephone: "(+233) 54 686 3012",
+  telephoneFormatted: "(+233) 54 686 3012".replace("(", "").replace(")", ""),
+};
+
+export default legal;
