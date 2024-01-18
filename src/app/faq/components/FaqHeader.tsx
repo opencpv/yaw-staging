56dfac2f5becc0e7f493f7af093ef70d2bdb245a
Enoch Ansah <enocansah@yahoo.com>
2024-01-18 13:26:48 +0000

    Add: styling to section element for consistency

--- src/app/faq/components/FaqHeader.tsx
+++ src/app/faq/components/FaqHeader.tsx
@@ -7,28 +7,26 @@ type Props = {};
 
 const FaqHeader = (
   props: Props,
-  ref:
-    | React.RefObject<HTMLDivElement>
-    | React.ForwardedRef<HTMLDivElement>
+  ref: React.RefObject<HTMLDivElement> | React.ForwardedRef<HTMLDivElement>,
 ) => {
   const { images } = useAssets();
   return (
-      <div
-        className={`flex flex-wrap justify-between green-gradient h-fit banner px-10 pt-10 gap-x-5 gap-y-20 text-white -mx-5 xs:rounded-3xl xs:mx-0 md:pt-24 sm:px-24`}
-        ref={ref}
-      >
-        <div className="space-y-4">
-          <h1 className="sm:text-5xl">FAQ</h1>
-          <h2>Everything You Need to Know</h2>
-        </div>
-          <Image
-            src={images.FaqImage}
-            alt="Four people around a giant FAQ graphic"
-            width={400}
-            // height={400}
-            className=" md:mb-10 lg:mb-5"
-          />
+    <div
+      className={`green-gradient banner -mx-5 flex h-fit flex-wrap justify-between gap-x-5 gap-y-20 px-10 pt-10 text-white xs:mx-0 xs:rounded-3xl sm:px-24 md:pt-24`}
+      ref={ref}
+    >
+      <div className="space-y-4">
+        <h1 className="sm:text-5xl">FAQ</h1>
+        <h2>Everything You Need to Know</h2>
       </div>
+      <Image
+        src={images.FaqImage}
+        alt="Four people around a giant FAQ graphic"
+        width={400}
+        // height={400}
+        className=" md:mb-10 lg:mb-5"
+      />
+    </div>
   );
 };
 
