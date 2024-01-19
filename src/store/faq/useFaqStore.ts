408b7e1cf6934f47707300705e4aa30dc0f00e14
Enoch Ansah <enocansah@yahoo.com>
2024-01-19 18:41:44 +0000

    Update: faq browser and items

--- src/store/faq/useFaqStore.ts
+++ src/store/faq/useFaqStore.ts
@@ -2,13 +2,18 @@ import React from "react";
 import { create } from "zustand";
 
 type FaqHowToSwitchStore = {
-  activePage: React.Key
+  activePage: React.Key;
   setActivePage: (page: React.Key) => void;
 };
 
 type HowToTabsStore = {
-  activeTab: React.Key
+  activeTab: React.Key;
   setActiveTab: (page: React.Key) => void;
+};
+
+type FaqStore = {
+  activeBrowser: number;
+  setActiveBrowser: (value: number) => void;
 };
 
 export const useFaqHowToSwitchStore = create<FaqHowToSwitchStore>((set) => ({
@@ -19,4 +24,10 @@ export const useFaqHowToSwitchStore = create<FaqHowToSwitchStore>((set) => ({
 export const useHowToTabsStore = create<HowToTabsStore>((set) => ({
   activeTab: "all",
   setActiveTab: (page) => set({ activeTab: page }),
+}));
+
+export const useFaqStore = create<FaqStore>((set) => ({
+  activeBrowser: 0,
+  setActiveBrowser: (value) =>
+    set((state) => ({ ...state, activeBrowser: value })),
 }));
