da8731084a442ca184033ad0ae782bf8421bc40f
Enoch Ansah <enocansah@yahoo.com>
2024-01-20 02:34:01 +0000

    Update: contact page banner

--- src/app/contact/components/ContactBanner.tsx
+++ src/app/contact/components/ContactBanner.tsx
@@ -6,20 +6,19 @@ type Props = {};
 const ContactBanner = (props: Props, ref: LegacyRef<HTMLDivElement>) => {
   return (
     <div
-      className={`banner -mx-5 flex flex-col items-center justify-center gap-5 px-10 pt-10 text-white xs:mx-0 sm:px-24 md:pt-24 lg:flex-row lg:items-start lg:justify-between`}
+      className={`banner -mx-5 flex flex-col items-center justify-center gap-5 px-10 pt-10 text-white xs:mx-0 xs:w-full sm:px-24 md:pt-24 lg:flex-row lg:items-start lg:justify-between`}
       ref={ref}
     >
       <div className="">
         <h1 className="text-center">Get in touch with us</h1>
       </div>
-      <div className="relative aspect-square w-52 min-[340px]:w-96 lg:bottom-32 lg:pb-5">
-        <Image
-          src={"/svgs/contact1.png"}
-          alt="Customer service personnel"
-          fill
-          style={{ objectFit: "contain" }}
-        />
-      </div>
+      <Image
+        src={"/svgs/contact1.png"}
+        alt="Customer service personnel"
+        width={320}
+        height={320}
+        className="relative mb-10 lg:bottom-20 lg:mb-0"
+      />
     </div>
   );
 };
