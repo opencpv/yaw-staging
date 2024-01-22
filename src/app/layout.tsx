b8c1ee84adb9d8b67140152fb7dde9e5b540e649
Enoch Ansah <enocansah@yahoo.com>
2024-01-22 03:33:42 +0000

    Add: Head component in faq page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "../styles/animations.css";
 import "@radix-ui/themes/styles.css";
 import Providers from "@/context/Providers";
 import LoadingIndicator from "@/components/LoadingIndicator";
-import Head from "next/head";
 import Script from "next/script";
 import { openSans } from "@/lib/utils/fonts";
 import { ToastContainer } from "react-toastify";
@@ -52,12 +51,6 @@ export default function RootLayout({
       lang="en"
       className="text-[14px] lg:text-[14.5px] 2xl:text-[15px] 3xl:text-[16px]"
     >
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-      </Head>
       <Script src="https://widget.cloudinary.com/v2.0/global/all.js" />
       <body className={`bg-white text-neutral-800  ${openSans.className}`}>
         <Providers>
