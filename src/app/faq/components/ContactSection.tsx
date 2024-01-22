8fa6b77181c137b9551213df88f62003f8929244
Enoch Ansah <enocansah@yahoo.com>
2024-01-22 03:33:42 +0000

    Add: Head component in faq page

--- src/app/faq/components/ContactSection.tsx
+++ src/app/faq/components/ContactSection.tsx
@@ -1,21 +1,30 @@
 import ContactForm from "./ContactForm";
 import Image from "next/image";
+import Head from "next/head";
 import { useAssets } from "@/lib/custom-hooks/useAssets";
 
 const ContactSection = () => {
   const { images } = useAssets();
   return (
-    <section className="section space-y-8">
-      <h2 className="sm:text-3xl">Have more Questions?</h2>
-      <div className="flex flex-col gap-10 gap-y-20 md:flex-row">
-        <div className="flex-1">
-          <ContactForm />
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <section className="section space-y-8">
+        <h2 className="sm:text-3xl">Have more Questions?</h2>
+        <div className="flex flex-col gap-10 gap-y-20 md:flex-row">
+          <div className="flex-1">
+            <ContactForm />
+          </div>
+          <div className="flex-1">
+            <Image src={images.ContactImage} alt="contact image" width={1200} />
+          </div>
         </div>
-        <div className="flex-1">
-          <Image src={images.ContactImage} alt="contact image" width={1200} />
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
