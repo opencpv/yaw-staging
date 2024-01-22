05d390df6537877baaa7679353f1dcefa707cfac
Enoch Ansah <enocansah@yahoo.com>
2024-01-22 13:01:45 +0000

    Update: converted @/app/layout to a server component for SEO improvement

--- src/app/faq/components/ContactSection.tsx
+++ src/app/faq/components/ContactSection.tsx
@@ -1,21 +1,31 @@
 import ContactForm from "./ContactForm";
 import Image from "next/image";
 import { useAssets } from "@/lib/custom-hooks/useAssets";
-import { Viewport } from "next/dist/lib/metadata/types/extra-types";
+// import type { Viewport } from "next";
+import type { Metadata } from "next";
+import Head from "next/head";
 
-export const viewport: Viewport = {
-  // width: 'device-width',
-  // initialScale: 1,
-  maximumScale: 1,
-  // userScalable: false,
-  // Also supported by less commonly used
-  // interactiveWidget: 'resizes-visual',
+// export const viewport = {
+//   // width: 'device-width',
+//   // initialScale: 1,
+//   maximumScale: 1,
+//   // userScalable: false,
+//   // Also supported by less commonly used
+//   // interactiveWidget: 'resizes-visual',
+// };
+
+export const metadata: Metadata = {
+  title: "FAQ",
+  description: "FAQ page",
 };
 
 const ContactSection = () => {
   const { images } = useAssets();
   return (
     <section className="section space-y-8">
+      {/* <Head>
+        <title>FAQ</title>
+      </Head> */}
       <h2 className="sm:text-3xl">Have more Questions?</h2>
       <div className="flex flex-col gap-10 gap-y-20 md:flex-row">
         <div className="flex-1">
