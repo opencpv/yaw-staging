5a7227c25931ff572a22e76ac8491f54e3f12b87
Enoch Ansah <enocansah@yahoo.com>
2024-01-22 03:52:55 +0000

    Add: viewport maximumScale for meta tag

--- src/app/faq/components/ContactSection.tsx
+++ src/app/faq/components/ContactSection.tsx
@@ -1,30 +1,31 @@
 import ContactForm from "./ContactForm";
 import Image from "next/image";
-import Head from "next/head";
 import { useAssets } from "@/lib/custom-hooks/useAssets";
+import { Viewport } from "next/dist/lib/metadata/types/extra-types";
+
+export const viewport: Viewport = {
+  // width: 'device-width',
+  // initialScale: 1,
+  maximumScale: 1,
+  // userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+};
 
 const ContactSection = () => {
   const { images } = useAssets();
   return (
-    <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-      </Head>
-      <section className="section space-y-8">
-        <h2 className="sm:text-3xl">Have more Questions?</h2>
-        <div className="flex flex-col gap-10 gap-y-20 md:flex-row">
-          <div className="flex-1">
-            <ContactForm />
-          </div>
-          <div className="flex-1">
-            <Image src={images.ContactImage} alt="contact image" width={1200} />
-          </div>
+    <section className="section space-y-8">
+      <h2 className="sm:text-3xl">Have more Questions?</h2>
+      <div className="flex flex-col gap-10 gap-y-20 md:flex-row">
+        <div className="flex-1">
+          <ContactForm />
         </div>
-      </section>
-    </>
+        <div className="flex-1">
+          <Image src={images.ContactImage} alt="contact image" width={1200} />
+        </div>
+      </div>
+    </section>
   );
 };
 
