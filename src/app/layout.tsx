382b4a0724dd90c88e84c61eb74434a2c25b6ee7
Enoch Ansah <enocansah@yahoo.com>
2024-01-22 01:03:16 +0000

    Update: viewport meta tag to fix scaling on IOS devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "../styles/animations.css";
 import "@radix-ui/themes/styles.css";
 import Providers from "@/context/Providers";
 import LoadingIndicator from "@/components/LoadingIndicator";
+import Head from "next/head";
 import Script from "next/script";
 import { openSans } from "@/lib/utils/fonts";
 import { ToastContainer } from "react-toastify";
@@ -51,6 +52,13 @@ export default function RootLayout({
       lang="en"
       className="text-[14px] lg:text-[14.5px] 2xl:text-[15px] 3xl:text-[16px]"
     >
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+          key="page-scale"
+        />
+      </Head>
       <Script src="https://widget.cloudinary.com/v2.0/global/all.js" />
       <body className={`bg-white text-neutral-800  ${openSans.className}`}>
         <Providers>
