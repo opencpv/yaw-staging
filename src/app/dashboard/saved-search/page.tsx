a02fed9f917b1cacaed0d0106ca572448daa2327
Enoch Ansah <enocansah@yahoo.com>
2024-01-28 00:36:33 +0000

    Update: change background image on saved search results card

--- src/app/dashboard/saved-search/page.tsx
+++ src/app/dashboard/saved-search/page.tsx
@@ -3,22 +3,16 @@ import Image from "next/image";
 import { useState } from "react";
 import SavedSearchCard from "./components/SavedSearchCard";
 import { useAssets } from "@/lib/custom-hooks/useAssets";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Page() {
   const [savedSearches, setSavedSearches] = useState(true);
   const { images } = useAssets();
-
   return (
     <div className="flex w-full flex-col items-center justify-center ">
       {!savedSearches && (
         <div className="mt-24 flex w-full flex-col items-center justify-center pt-5">
           <div className="relative aspect-[411/282] w-full max-w-[411px]">
-            <Image src={images.StockImage} fill alt="No saved search" />
-            <FaMagnifyingGlass
-              size={48}
-              className="absolute left-[40%] top-[40%]"
-            />
+            <Image src={"/svgs/saved-search.svg"} fill alt="No saved search" />
           </div>
           <p className="text-[1.5625rem] font-semibold text-shade-300">
             You have no saved searches
