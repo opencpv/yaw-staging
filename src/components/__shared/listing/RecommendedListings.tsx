4e27e0734d257be43860cdb6c79900746b1f1b90
Enoch Ansah <enocansah@yahoo.com>
2024-01-29 14:38:17 +0000

    Update: gradient banner width and char limit of paid feature title

--- src/components/__shared/listing/RecommendedListings.tsx
+++ src/components/__shared/listing/RecommendedListings.tsx
@@ -12,8 +12,7 @@ import { fetchOrderRule, revalidationRule } from "@/lib/utils/fetchRules";
 import images from "@/enum/temp/images";
 import FetchErrorMessage from "../ui/data_fetching/FetchErrorMessage";
 import Button from "../ui/button/Button";
-import ArrowLink from "@/app/components/link/ArrowLink";
-import { useSliderAutoPlayDisclosure } from "@/lib/custom-hooks/useCustomDisclosure";
+import style from "./Listing.module.css";
 
 type Props = {
   className?: string;
@@ -37,60 +36,86 @@ const RecommendedListings = ({ className, showAllButton }: Props) => {
   );
 
   return (
-    <section className={`no-print h-fit w-full ${className}`}>
-      <div className="mb-6 flex flex-wrap items-center justify-between gap-5">
-        <h2>Recommended Listings</h2>
-        <Button
-          href="/properties"
-          variant="ghost"
-          className={`text-sm text-neutral-800 ${
-            showAllButton ? "block" : "hidden"
-          } ${isLoading && "hidden"}`}
-        >
-          Show all
-        </Button>
-      </div>
-      <FetchingStates
-        data={listings}
-        error={error}
-        isLoading={isLoading}
-        isValidating={isValidating}
-        isLoadingComponent={
-          <div className="flex gap-5 overflow-x-hidden">
-            <SkeletonListing count={5} childrenClassName="w-96" />
-          </div>
+    <>
+      <section className={`no-print h-fit w-full ${className}`}>
+        <div className="mb-6 flex flex-wrap items-center justify-between gap-5">
+          <h2>Recommended Listings</h2>
+          <Button
+            href="/properties"
+            variant="ghost"
+            className={`text-sm text-neutral-800 ${
+              showAllButton ? "block" : "hidden"
+            } ${isLoading && "hidden"}`}
+          >
+            Show all
+          </Button>
+        </div>
+        <FetchingStates
+          data={listings}
+          error={error}
+          isLoading={isLoading}
+          isValidating={isValidating}
+          isLoadingComponent={
+            <div className="flex gap-5 overflow-x-hidden">
+              <SkeletonListing count={5} childrenClassName="w-96" />
+            </div>
+          }
+          errorComponent={<FetchErrorMessage />}
+          noDataMessageComponent={
+            <p className="mt-4 text-center italic">
+              There are no properties yet.
+            </p>
+          }
+        />
+        {
+          <SliderMultiItems
+            autoplay
+            hasNavAndPagination={false}
+            slidesPerView={1.5}
+            breakpoints={{
+              500: {
+                slidesPerView: 1.5,
+              },
+              768: {
+                slidesPerView: 2.5,
+              },
+              1200: {
+                slidesPerView: 3,
+              },
+              1300: {
+                slidesPerView: 3.5,
+              },
+              1536: {
+                slidesPerView: 4,
+              },
+            }}
+            swiperSlideClassName="max-w-[23rem] min-w-[23rem] h-full"
+            items={listings?.map((listing) => (
+              <ListingCard
+                key={listing.id}
+                href={`/properties/${listing.property_id}`}
+                propertyName={listing.property_name as string}
+                city={listing.city as string}
+                images={images} // TODO: check database
+                liked={false} // TODO: check implementation
+                membership={"Certified" as Membership} // TODO: check database
+                monthlyAmount={listing.monthly_amount as number}
+                paymentStructure={"Bi-Annually" as PaymentStructure} // TODO: check database
+                propertyDescription={listing.description as string}
+                price={4000} // TODO: check database
+                rating={4.5} // TODO: check database
+                ratingCount={105} // TODO: check database
+                deal={"Best Value" as Deal} // TODO: check database
+              />
+            ))}
+          />
         }
-        errorComponent={<FetchErrorMessage />}
-        noDataMessageComponent={
-          <p className="mt-4 text-center italic">
-            There are no properties yet.
-          </p>
-        }
-      />
-      {
-        <SliderMultiItems
-          autoplay
-          hasNavAndPagination={false}
-          slidesPerView={1.5}
-          breakpoints={{
-            500: {
-              slidesPerView: 1.5,
-            },
-            768: {
-              slidesPerView: 2.5,
-            },
-            1200: {
-              slidesPerView: 3,
-            },
-            1300: {
-              slidesPerView: 3.5,
-            },
-            1536: {
-              slidesPerView: 4,
-            },
-          }}
-          swiperSlideClassName="max-w-[23rem] min-w-[23rem] h-full"
-          items={listings?.map((listing) => (
+      </section>
+
+      {/* <section className="relative mt-20 h-fit">
+        <h2 className="mb-6">Recommended Listing</h2>
+        <div className={style.marquee}>
+          {listings?.map((listing) => (
             <ListingCard
               key={listing.id}
               href={`/properties/${listing.property_id}`}
@@ -106,11 +131,12 @@ const RecommendedListings = ({ className, showAllButton }: Props) => {
               rating={4.5} // TODO: check database
               ratingCount={105} // TODO: check database
               deal={"Best Value" as Deal} // TODO: check database
+              className="w-[23rem] min-w-[23rem]"
             />
           ))}
-        />
-      }
-    </section>
+        </div>
+      </section> */}
+    </>
   );
 };
 
