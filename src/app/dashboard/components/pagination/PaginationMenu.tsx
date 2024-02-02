67390a7f61949b2af709e4c63f8077a6718ce7cc
Enoch Ansah <enocansah@yahoo.com>
2024-02-02 13:53:38 +0000

    Update: scroll down btn on pag.menu

--- src/app/dashboard/components/pagination/PaginationMenu.tsx
+++ src/app/dashboard/components/pagination/PaginationMenu.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useEffect, useRef, useState } from "react";
 import style from "../../Dashboard.module.css";
 import Logo from "@/components/__shared/Logo";
@@ -9,28 +8,67 @@ import { useHideDocumentScrollBar } from "@/lib/custom-hooks/useWindowEvents";
 import PaginationMenuItem from "./PaginationMenuItem";
 import { PgRoutesRenter } from "./links";
 import MenuScrollDownButton from "@/components/NavMenu.tsx/components/MenuScrollDownButton";
+import { pagination } from "@nextui-org/react";
 
 const PaginationMenu = () => {
   const { isOpen, setIsOpen } = useDashboardMenuStore();
   const [hide, setHide] = useState(false);
 
-  const paginationMenuRef = useRef<HTMLElement>(null)
+  const paginationMenuRef = useRef<HTMLElement>(null);
 
   useHideDocumentScrollBar(isOpen);
 
+  const handleScrollDown = () => {
+    if (paginationMenuRef.current) {
+      paginationMenuRef.current.scrollTo({
+        top:
+          paginationMenuRef.current.scrollHeight -
+          paginationMenuRef.current.offsetHeight,
+        behavior: "smooth",
+      });
+      setHide(true);
+    }
+  };
+
   useEffect(() => {
     const handleResize = () => {
-      if (paginationMenuRef.current) {
-      if (paginationMenuRef.current.scrollHeight < 690 ){
-        setHide(false)
-      }
-      else {
-        setHide(true)
-      }
-    } }
-    if (paginationMenuRef.current)
-    paginationMenuRef.current.addEventListener("resize", handleResize)
-  }, [hide])
+      if (paginationMenuRef.current)
+        if (
+          paginationMenuRef.current.scrollHeight -
+            paginationMenuRef.current.offsetHeight <=
+          80
+        ) {
+          setHide(true);
+        }
+    };
+
+    const handleOnMount = () => {
+      if (paginationMenuRef.current)
+        if (
+          paginationMenuRef.current.scrollTop >=
+          paginationMenuRef.current.scrollHeight -
+            paginationMenuRef.current.offsetHeight -
+            80
+        ) {
+          setHide(true);
+        }
+    };
+
+    const handleScroll = () => {
+      if (paginationMenuRef.current)
+        if (paginationMenuRef.current.scrollTop > 0) {
+          setHide(true);
+        }
+    };
+
+    handleOnMount();
+    window.addEventListener("resize", handleResize);
+    paginationMenuRef?.current?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <section
@@ -62,12 +100,12 @@ const PaginationMenu = () => {
           ))}
         </div>
       </div>
-        <MenuScrollDownButton
-          onClick={() => console.log("hi")}
-          hide={hide}
-          isInViewport={false}
-          className="absolute right-40 bottom-5 block"
-        />
+      <MenuScrollDownButton
+        onClick={handleScrollDown}
+        hide={hide}
+        isInViewport={false}
+        className="min-[700]:right-14 min-[700]:bottom-5 absolute bottom-3 right-3 block xl:right-40"
+      />
     </section>
   );
 };
