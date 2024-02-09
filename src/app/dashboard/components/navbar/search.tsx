9d6c9eec2de8535eec6f1e23b30cffc376a428ab
Enoch Ansah <enocansah@yahoo.com>
2024-02-09 17:05:16 +0000

    Fix: build errors

--- src/app/dashboard/components/navbar/search.tsx
+++ src/app/dashboard/components/navbar/search.tsx
@@ -1,10 +1,14 @@
 import InputWithSavedSearch from "@/components/__shared/form/InputWithSavedSearch";
+import { cn } from "@/lib/utils";
 import React from "react";
 
-const Search = () => {
+const Search = ({ className }: { className?: string }) => {
   return (
     <InputWithSavedSearch
-      className="h-[52px] max-h-[52px] max-w-lg rounded-[4px] bg-[#F9F9F9] md:h-[42px] xl:min-w-[420px] 2xl:aspect-[620/52] 2xl:h-[52px]"
+      className={cn(
+        "h-[52px] max-h-[52px] max-w-lg rounded-[4px] bg-[#F9F9F9] md:h-[42px] xl:min-w-[420px] 2xl:aspect-[620/52] 2xl:h-[52px]",
+        className,
+      )}
       inputClassName="rounded-[4px] border-0 px-4 py-1"
       // separatorClassName="right-[20.5%] top-3 xs:right-[19%] md:right-[18%] md:top-2"
     />
