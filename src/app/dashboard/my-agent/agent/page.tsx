0ac52632fe0c4464741c3b834708b691353a9b44
Enoch Ansah <enocansah@yahoo.com>
2024-02-10 15:40:27 +0000

    Update: agent card

--- src/app/dashboard/my-agent/agent/page.tsx
+++ src/app/dashboard/my-agent/agent/page.tsx
@@ -5,6 +5,7 @@ import NoMatchesYet from "../components/NoMatches";
 import MatchesYet from "../components/Matches";
 import Agent from "../components/Agent";
 import AOSWrapper from "@/components/__shared/AOSWrapper";
+import NoAgentCard from "../components/NoAgentCard";
 
 export default function Page() {
   const [beAgent, setBeAgent] = useState(false);
@@ -13,34 +14,16 @@ export default function Page() {
     <div className="mx-auto mt-16 flex max-w-screen-3xl flex-col items-center justify-center gap-8 px-5 sm:px-10">
       {!beAgent && (
         <div className="flex w-full flex-col">
-          <AOSWrapper animation="fade-up" className="flex flex-col gap-5 ">
-            <div className="flex w-fit flex-col items-start ">
-              <div className="flex w-full max-w-[542px] flex-col gap-8">
-                <h3 className="">My Agent</h3>
-                <div className="flex max-w-md flex-col gap-[1.5rem] rounded-2xl border-[1px] border-[#E6E6E6] bg-[#F7F7F7] px-8 py-6 xs:w-[448px]">
-                  <div className="flex flex-col gap-[2px]">
-                    <h3>You have no agent</h3>
-                    <p className="text-shade-200">
-                      Click the button below to get started
-                    </p>
-                  </div>
-                  <div className="flex w-full flex-col gap-6 xs:flex-row xs:items-center">
-                    <AgentButtons
-                      content="Get Started"
-                      variant={"green-dark"}
-                      onClick={() => setBeAgent(false)} // Implementation has changed !!
-                    />
-                    <AgentButtons
-                      content="Explore"
-                      variant={"explore"}
-                      className="min-w-[10.5rem] xs:min-w-[8rem]"
-                      onClick={() => setBeAgent(false)} // Implementation has changed !!
-                    />
-                  </div>
-                </div>
-              </div>
+          <AOSWrapper animation="fade-up" className="flex flex-col gap-5">
+            <div className="w-full max-w-[542px] space-y-8">
+              <h3 className="">My Agent</h3>
+              <NoAgentCard />
             </div>
-            <Agent />
+            <div className="flex flex-wrap gap-5">
+              <Agent name="My Agent One" state="completed" dateCreated="5 Aug. 2023 13:55pm" dateCompleted="15 Aug. 2023 13:55pm" />
+              <Agent name="My Agent Two" state="started" dateCreated="5 Aug. 2023 13:55pm" />
+              <Agent name="My Agent Three" state="not started" />
+            </div>
           </AOSWrapper>
           {!matches && <NoMatchesYet />}
           {matches && <MatchesYet />}
--- src/app/dashboard/my-agent/components/Agent.tsx
+++ src/app/dashboard/my-agent/components/Agent.tsx
@@ -1,30 +1,73 @@
 import Image from "next/image";
 import EditModal from "./EditModal";
 import CaAgentTickGreenBg from "./icons/CaAgentTickGreenBg";
+import AgentButtons from "./Button";
+import Link from "next/link";
+import Button from "@/components/__shared/ui/button/Button";
 
-export default function Agent() {
+type Props = {
+  state: "started" | "not started" | "completed";
+  name: string;
+  dateCreated?: string;
+  dateCompleted?: string;
+};
+
+export default function Agent({
+  state,
+  name,
+  dateCreated,
+  dateCompleted,
+}: Props) {
   return (
     <div className="flex h-full w-full max-w-[543px] flex-col items-start justify-center gap-4 rounded-2xl border border-[#E6E6E6] px-8 py-6">
-      <div className="flex w-full justify-between">
-        <div className="relative aspect-square w-full max-w-[112px]">
-          <Image
-            src={"/assets/svgs/agent-icon-handshake.svg"}
-            fill
-            alt="Agent Icon"
+      <div className="flex w-full flex-wrap gap-5 justify-between">
+        <Image
+          src={"/assets/svgs/agent-icon-handshake.svg"}
+          width={100}
+          height={100}
+          alt="A house inside a hand"
+          className="h-[70px] w-[70px] sm:h-[100px] sm:w-[100px]"
+        />
+        {state === "not started" ? (
+          <AgentButtons content="Get Started" variant={"green-dark"} />
+        ) : state === "completed" ? (
+          <AgentButtons
+            content="View"
+            variant={"explore"}
+            className="min-w-fit xsm:min-w-[8rem]"
           />
-        </div>
-        <EditModal />
+        ) : (
+          <EditModal />
+        )}
       </div>
       <div className="space-y-1">
-        <h3 className="font-semibold">My Agent one</h3>
-        <p className="text-shade-200">Date Created : 15 Aug. 2023 13:55pm</p>
-        <div className="flex flex-wrap items-center gap-x-4 gap-y-2 pt-4 xs:pt-2">
-          <div className="flex items-center gap-2 rounded-full bg-[#F1F1F1] px-2 py-1.5">
-            <CaAgentTickGreenBg />
-            <p>Completed</p>
+        <h3 className="font-semibold">{name}</h3>
+        {state === "not started" ? (
+          <div className="group flex w-fit flex-col gap-1">
+            <Button variant="ghost" href="" className="mt-2 flex-1">
+              Not started
+            </Button>
+            <div className="h-0 w-0 flex-1 border-b border-neutral-800 transition-all group-hover:h-[1px] group-hover:w-full"></div>
           </div>
-          <p className="text-shade-200">5 Aug. 2023 13:55pm</p>
-        </div>
+        ) : state === "started" ? (
+          <div className="group flex w-fit flex-col gap-1">
+            <Button variant="ghost" href="" className="mt-2 flex-1">
+              Continue
+            </Button>
+            <div className="h-0 w-0 flex-1 border-b border-neutral-800 transition-all group-hover:h-[1px] group-hover:w-full"></div>
+          </div>
+        ) : (
+          <>
+            <p className="text-shade-200">Date Created : {dateCreated}</p>
+            <div className="flex flex-wrap items-center gap-x-4 gap-y-2 pt-4 xs:pt-2">
+              <div className="flex items-center gap-2 rounded-full bg-[#F1F1F1] px-2 py-1.5">
+                <CaAgentTickGreenBg />
+                <p>Completed</p>
+              </div>
+              <p className="text-shade-200">{dateCompleted}</p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
