0da291cfb7ab062d11831b2e03f769e5fcbf07cc
opencpv <sulemanaadams375@gmail.com>
2024-02-13 12:57:48 +0000

    added otp and sms apis

---
+++ src/app/api/otp/resend/resend.ts
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const resend = async (requestId: string) => {
+    const response = await axios.post(
+        `https://api-devp-otp-2704.hubtel.com/otp/resend`,
+        { requestId },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Basic ' + Buffer.from(`${process.env.HUBTEL_CLIENT_ID}:${process.env.HUBTEL_CLIENT_SECRET}`).toString('base64')
+            }
+        }
+    );
+
+    console.log(response.data);
+
+}
+
+export default resend
