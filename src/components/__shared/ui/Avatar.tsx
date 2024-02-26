d0e4b7caf387b328315fdcba49cdce818cf9a124
Enoch Ansah <enocansah@yahoo.com>
2024-02-26 23:00:06 +0000

    Update: adjust padding on avatar menu

--- src/components/__shared/ui/Avatar.tsx
+++ src/components/__shared/ui/Avatar.tsx
@@ -57,7 +57,7 @@ const Avatar = ({ image, name, className }: Props) => {
           isOpen ? `${style.avatarMenuVisible}` : `${style.avatarMenuHidden}`
         }`}
       >
-        <ul className="divide-y">
+        <ul>
           <li className="flex gap-4 p-5">
             <Image
               src={image}
@@ -74,34 +74,34 @@ const Avatar = ({ image, name, className }: Props) => {
               <small className="text-shade-300">enocansah@gmail.com</small>
             </div>
           </li>
-          <li className="space-y-5 p-5 pl-4">
+          <hr className="mx-5" />
+          <li className="deep-green-hover pl-8 pr-4">
             {/* My Account */}
             {pathname?.includes("dashboard") ? (
-              <Link
-                href="/"
-                className="deep-green-hover flex items-center gap-2"
-              >
+              <Link href="/" className="flex items-center gap-2 pb-2.5 pt-4">
                 <MdOutlineHome size={20} />
                 <p>Home</p>
               </Link>
             ) : (
               <Link
                 href={`/dashboard/${currentRole}/overview`}
-                className="deep-green-hover flex items-center gap-2"
+                className="flex items-center gap-2 pb-2.5 pt-4"
               >
                 <FaRegUser size={20} />
                 <p>My Account</p>
               </Link>
             )}
-
-            {/* Settings */}
-            <Link href="" className="deep-green-hover flex items-center gap-2">
+          </li>
+          {/* Settings */}
+          <li className="deep-green-hover pl-8 pr-4">
+            <Link href="" className="flex items-center gap-2 pb-4 pt-2.5">
               <LuSettings size={20} />
               <p>Settings</p>
             </Link>
           </li>
+          <hr className="mx-5" />
           {/* Logout */}
-          <li className="space-y-5 px-5 pb-1 pl-4 pt-10">
+          <li className="deep-green-hover cursor-pointer space-y-5 py-4 pb-5 pl-8 pr-4 pt-10">
             <div className="flex items-center gap-2">
               <TbLogout size={20} />
               <p>Log Out</p>
