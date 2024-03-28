f339265102d68c44bef9364edffa06fd882f1b9c
opencpv <sulemanaadams375@gmail.com>
2024-03-28 12:27:09 +0000

    integrated google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,19 @@ export default async function RootLayout({
       lang="en"
       className="text-[14px] lg:text-[14.5px] 2xl:text-[15px] 3xl:text-[16px]"
     >
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HE2ZM0VHPH"
+        ></Script>
+        <Script id="google-analytics">
+          {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HE2ZM0VHPH');
+            `}
+        </Script>
+      </head>
       <Script src="https://widget.cloudinary.com/v2.0/global/all.js" />
 
       <body className={`bg-white text-neutral-800 light ${openSans.className}`}>
