42868d2354d09439b9ee33e913d86db3efe4f4d0
opencpv <sulemanaadams375@gmail.com>
2024-03-31 21:08:10 +0000

    fixed blog page

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -28,32 +28,36 @@ const page = async (props: Props) => {
 
   return (
     <>
-      <div className="relative mb-10 flex h-60 w-full items-center justify-center bg-gradient-to-b from-primary-500 to-primary-500/80 text-white lg:h-[27rem]">
-        <BackgroundImage />
-        <div className="flex flex-col items-center gap-3">
-          <div className="flex items-center gap-3">
-            <Link href="/">Home</Link>
-            <LuChevronsRight />
-            <Link href="/blog">Blogs</Link>
+      {categories && posts && (
+        <>
+          <div className="relative mb-10 flex h-60 w-full items-center justify-center bg-gradient-to-b from-primary-500 to-primary-500/80 text-white lg:h-[27rem]">
+            <BackgroundImage />
+            <div className="flex flex-col items-center gap-3">
+              <div className="flex items-center gap-3">
+                <Link href="/">Home</Link>
+                <LuChevronsRight />
+                <Link href="/blog">Blogs</Link>
+              </div>
+              <AOSWrapper animation="fade-up" duration="2000">
+                <h1 className="text-5xl font-[700]">Blog</h1>
+              </AOSWrapper>
+            </div>
           </div>
-          <AOSWrapper animation="fade-up" duration="2000">
-            <h1 className="text-5xl font-[700]">Blog</h1>
-          </AOSWrapper>
-        </div>
-      </div>
-      <div className="wrapper">
-        <div className="mb-10 flex flex-col items-center justify-between gap-5 sm:flex-row">
-          <TabsAndSearch
-            categories={categories.map(
-              (category: any) => category.category_title,
-            )}
-          />
-        </div>
-        <div className="flex items-center justify-center pb-10 text-neutral-500">
-          <PostsGrid posts={posts} />
-        </div>
-        {/* <p className="mb-20 text-center">pagination</p> */}
-      </div>
+          <div className="wrapper">
+            <div className="mb-10 flex flex-col items-center justify-between gap-5 sm:flex-row">
+              <TabsAndSearch
+                categories={categories.map(
+                  (category: any) => category.category_title,
+                )}
+              />
+            </div>
+            <div className="flex items-center justify-center pb-10 text-neutral-500">
+              <PostsGrid posts={posts} />
+            </div>
+            {/* <p className="mb-20 text-center">pagination</p> */}
+          </div>
+        </>
+      )}
     </>
   );
 };
