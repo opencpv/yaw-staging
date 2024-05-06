34ca44344095cb1fcf2824d2fc23e25fbc1ced49
YawOtuo <yotuo2003@gmail.com>
2024-05-06 17:08:30 +0000

    Update JobDescriptionModalContent: increase Share component font size
    This commit updates the JobDescriptionModalContent component by increasing the font size of the Share component. The line of code that sets the className of the Share component has been modified to include "

--- src/app/join-us/components/JobDescriptionModalContent.tsx
+++ src/app/join-us/components/JobDescriptionModalContent.tsx
@@ -26,14 +26,14 @@ function JobDescriptionModalContent({ job }: any) {
             <Share
               url={`https://rentrightgh.com${pathname}`}
               title={job?.title}
-              className="text-neutral-800"
+              className="text-neutral-800 text-2xl"
               //make it 24 px
             />
           </div>
           <div className="relative mt-2 flex flex-col gap-3 rounded-2xl border-[1px] border-shade-50 bg-[#FAFAFA] px-4 lg:px-8 py-2">
             <div className="download flex flex-col gap-1">
               <p className="border-b-[1px] border-shade-50 py-3 text-[1.5rem] font-semibold text-shade-300">
-                <span className="capitalize">{job?.title} ggg</span>
+                <span className="capitalize">{job?.title}</span>
               </p>
               <p className="hidden-scrollbar h-[70vh]  overflow-y-scroll text-shade-300 pt-3">
                 <PortableText value={job?.description} />
