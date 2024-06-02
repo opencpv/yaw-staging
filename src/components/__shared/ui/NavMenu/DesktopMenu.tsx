7f5d53b6f36a45950e33e88eca5381d5a36b7dae
YawOtuo <yotuo2003@gmail.com>
2024-06-02 20:50:14 +0000

    desktop menu edit

--- src/components/__shared/ui/NavMenu/DesktopMenu.tsx
+++ src/components/__shared/ui/NavMenu/DesktopMenu.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Separator from "../../../Separator";
 import { FadeInOut } from "@/lib/animations";
@@ -10,19 +10,30 @@ import { useAppStore } from "@/store/dashboard/AppStore";
 import { useMenuLinks } from "./content";
 import { createUUID } from "@/lib/utils/stringManipulation";
 import SubLinkResults from "./components/SubLinkResults";
+import { animate, stagger } from "framer-motion";
 
 export const DesktopMenu = (props: any) => {
   const { linksAfterLogin, linksBeforeLogin } = useMenuLinks();
   const [active, setActive] = useState<number | null>(null);
   const [subId, setSubId] = useState<number | null>(null);
   const router = useRouter();
-  const setToggle = useMenuStore((state) => state.setToggle);
+  const { setToggle, toggle } = useMenuStore();
   const { activeSubLink, setActiveSubLink } = useMenuStore();
   const { user } = useAppStore();
 
   const setFaqActivePage = useFaqHowToSwitchStore(
     (state) => state.setActivePage,
   );
+
+  useEffect(() => {
+    animate(
+      ".main-menu-link",
+      toggle ? { opacity: [0, 1], x: [-5, 0] } : { opacity: 1, x: 0 },
+      {
+        delay: stagger(0.1, { startDelay: 0.5 }),
+      },
+    );
+  }, [toggle]);
 
   return (
     <div className={`flex-row gap-12 px-8 ${props?.className} `}>
