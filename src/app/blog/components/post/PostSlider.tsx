4d1900fcb17205afc08b0c9bbf0b3d6f5ca5f07f
Enoch Ansah <enocansah@yahoo.com>
2024-06-13 21:57:49 +0000

    Fix build error

--- src/app/blog/components/post/PostSlider.tsx
+++ src/app/blog/components/post/PostSlider.tsx
@@ -31,13 +31,18 @@ const PostSlider = ({ posts }: Props) => {
         className="shape-polygon sm:h-[27rem]"
         onSlideChange={handleSlideChange}
       />
-       <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+      <svg className="flt_svg" xmlns="http://www.w3.org/2000/svg">
         <defs>
-            <filter id="flt_tag">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
-                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-            </filter>
+          <filter id="flt_tag">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="flt_tag"
+            />
+            <feComposite in="SourceGraphic" in2="flt_tag" operator="atop" />
+          </filter>
         </defs>
       </svg>
       <div className="absolute -left-10 bottom-5 z-10 space-y-2 px-20 text-white sm:bottom-20">
