ea3dc828865eb2e423cabac05cb1503940057433
YawOtuo <yotuo2003@gmail.com>
2024-06-18 22:35:23 +0000

    "Refactor MainView component: simplify layout and subtitle rendering"

--- src/app/terms-of-service/components/MainView.tsx
+++ src/app/terms-of-service/components/MainView.tsx
@@ -13,22 +13,14 @@ const MainView = ({ data }: { data: any }) => {
 
   return (
     <div className="flex  w-full flex-col items-center justify-center text-white">
-      <div className="flex lg:max-w-[60vw] flex-col gap-6 2xl:gap-12 items-center">
-        <div className="mb-4 flex flex-col items-center w-full ">
+      <div className="flex flex-col items-center gap-6 lg:max-w-[60vw] 2xl:gap-12">
+        <div className="mb-4 flex w-full flex-col items-center ">
           <h1 className="leading-tight">{data.title}</h1>
-          <h1 className="leading-tight text-white">
-            {words.length >= 3 ? (
-              <>
-                {words.slice(0, 2).join(" ")}{" "}
-                <span className="text-[#F4C86A]">{words[2]}</span>
-                {words.slice(3).length > 0 && " " + words.slice(3).join(" ")}
-              </>
-            ) : (
-              data.subtitle
-            )}
-          </h1>
+          <h1 className="leading-tight text-white">{data.subtitle}</h1>
         </div>
-        <div className={`${styles.termsMain} text-xl break-words 2xl:font-semibold`}>
+        <div
+          className={`${styles.termsMain} break-words text-xl 2xl:font-semibold`}
+        >
           <RichTextRenderer content={data.description[0]} />
         </div>
       </div>
