6ff1bdb3f5ab74fe077159c65871f6e0042e4fd0
Enoch Ansah <enocansah@yahoo.com>
2024-07-11 00:08:42 +0000

    feat(ui): update bma ui

--- src/app/dashboard/renter/my-agent/components/ActionButton.tsx
+++ src/app/dashboard/renter/my-agent/components/ActionButton.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Button from "@/components/__shared/ui/button/Button";
 
 type Props = {
-children: React.ReactNode  
-  title: string
+  children: React.ReactNode;
+  title: string;
 };
 
-export default function ScheduleVirtualTourModal(props: Props) {
+export default function ActionButton(props: Props) {
   return (
     <Button
       className={`h-10 w-full gap-2 rounded-2xl bg-secondary-500 font-semibold text-shade-200 hover:bg-primary-200 hover:text-white lg:h-14`}
