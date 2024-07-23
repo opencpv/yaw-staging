5a9e3e2fd35adef4d9e40d149d59d45b42e656c2
Enoch Ansah <enocansah@yahoo.com>
2024-07-23 18:56:39 +0000

    feat: update toast config

--- src/app/dashboard/renter/my-agent/components/MatchTable.tsx
+++ src/app/dashboard/renter/my-agent/components/MatchTable.tsx
@@ -77,7 +77,7 @@ export default function MatchTable() {
         <CallOut content="Lorem ipsum dolor sit amet consectetur. Consequat elementum consequat interdum integer imperdiet nisl. Ipsum eu eu tortor enim est mauris in sem. Eget dignissim risus diam consectetur magna. Non." />
       </div>
       {/* table */}
-      <Table className={cn({ "min-h-80 lg:hidden": !matches })}>
+      <Table className={cn("min-h-80", { "lg:hidden": !matches })}>
         <TableHeaderRow
           className="grid-cols-7 gap-16 lg:max-llg:gap-8"
           gap="2rem"
