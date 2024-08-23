b6e588e7da2d301dd39da26043bd1bb8e4f1bf7a
Enoch Ansah <enocansah@yahoo.com>
2024-08-23 07:59:56 +0000

    switch to local fonts

--- src/lib/utils/fonts.ts
+++ src/lib/utils/fonts.ts
@@ -1,48 +1,49 @@
-import { Open_Sans } from "next/font/google";
+//import { Open_Sans } from "next/font/google";
+//import { Pacifico } from "next/font/google";
 import Open_Sans_Local from "next/font/local";
-import Montserrat_Local from "next/font/local";
-import { Pacifico } from "next/font/google";
+import Pacifico_Local from "next/font/local";
 
-export const openSans = Open_Sans({
-  subsets: ["latin"],
+//export const openSans = Open_Sans({
+//  subsets: ["latin"],
+//  variable: "--font-open-sans",
+//  display: "swap",
+//  adjustFontFallback: false,
+//});
+
+//export const pacifico = Pacifico({
+//  subsets: ["latin"],
+//  variable: "--font-pacifico",
+//  weight: "400",
+//  preload: false,
+//  display: "swap",
+//  adjustFontFallback: false,
+//});
+
+export const openSans = Open_Sans_Local({
+  src: [
+    {
+      path: "../../../public/fonts/open_sans/OpenSans-VariableFont_wdth,wght.ttf",
+    },
+    {
+      path: "../../../public/fonts/open_sans/OpenSans-Italic-VariableFont_wdth,wght.ttf",
+      style: "italic",
+    },
+  ],
   variable: "--font-open-sans",
   display: "swap",
   adjustFontFallback: false,
+  fallback: ["Helvetica", "Arial", "sans-serif"],
 });
 
-export const pacifico = Pacifico({
-  subsets: ["latin"],
+export const pacifico = Pacifico_Local({
+  src: [
+    {
+      path: "../../../public/fonts/pacifico/Pacifico-Regular.ttf",
+    },
+  ],
   variable: "--font-pacifico",
-  weight: "400",
-  preload: false,
   display: "swap",
+  weight: "400",
   adjustFontFallback: false,
+  fallback: ["cursive"],
 });
-
-//export const openSansLocal = Open_Sans_Local({
-//  src: [
-//    {
-//      path: "../../../public/fonts/open_sans/OpenSans-VariableFont_wdth,wght.ttf",
-//    },
-//    {
-//      path: "../../../public/fonts/open_sans/OpenSans-Italic-VariableFont_wdth,wght.ttf",
-//      style: "italic",
-//    },
-//  ],
-//  variable: "--font-open-sans",
-//  display: "swap",
-//});
-
-//export const montserratLocal = Montserrat_Local({
-//  src: [
-//    {
-//      path: "../../../public/fonts/montserrat/Montserrat-VariableFont_wght.ttf",
-//    },
-//    {
-//      path: "../../../public/fonts/montserrat/Montserrat-Italic-VariableFont_wght.ttf",
-//      style: "italic",
-//    },
-//  ],
-//  variable: "--font-montserrat",
-//  display: "swap",
-//});
