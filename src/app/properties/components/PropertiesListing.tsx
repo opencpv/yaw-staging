8437e43cb2e30adfd72b7f2a7f056ac86d03d232
Enoch Ansah <enocansah@yahoo.com>
2024-09-14 00:50:23 +0000

    update listing page

--- src/app/properties/components/PropertiesListing.tsx
+++ src/app/properties/components/PropertiesListing.tsx
@@ -30,8 +30,8 @@ type Props = {
 const PropertiesListing = (props: Props) => {
   const { user } = useAppStore();
   const searchParams = useSearchParams();
-  const search = searchParams?.get("q") || "";
   const tag = searchParams?.get("tag") || "All";
+  const search = searchParams?.get("q") || "";
   const router = useRouter();
   const [showAd, setShowAd] = useState(false);
   const {
