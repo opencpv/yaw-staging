ef652051461d059d95a0dd4ea39aecf6cb9e8dd9
YawOtuo <yotuo2003@gmail.com>
2024-09-23 16:02:50 +0000

    "Refactor PageNotFound component: update layout, remove PageNotFoundDiscover component, and add PageNotFoundPill component"

--- src/components/__shared/ui/states/page-not-found/index.tsx
+++ src/components/__shared/ui/states/page-not-found/index.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "../../button";
 import Link from "next/link";
-import PageNotFoundDiscover from "./page-not-found-discover";
+import PageNotFoundDiscover from "./components/page-not-found-discover";
 import styles from "./index.module.css";
+import PageNotFoundPill from "./components/page-not-found-pill";
 
 type Props = {
   className?: string;
@@ -22,40 +23,45 @@ function PageNotFound({ className }: Props) {
       )}
     >
       <div
-        className={`grid w-full grid-cols-1 items-center justify-center gap-5 rounded-b-[80px] lg:rounded-b-[200px] bg-[#DEFAF9] p-20 ${styles.container} pb-[170px] lg:grid-cols-2`}
+        className={`flex flex-col lg:flex-row w-full items-center justify-around gap-5 rounded-b-[50px] bg-[#DEFAF9] p-20 lg:rounded-b-[200px] ${styles.container} lg:pb-[170px] `}
       >
-        <div className="flex flex-col gap-4">
-          <div className="w-fit rounded-lg bg-primary px-4 py-2 text-white">
-            <p>PAGE NOT FOUND</p>
-          </div>
-          <div className="flex flex-col gap-10 lg:gap-20">
-            <div className="flex flex-col gap-1">
-              <h1 className="capitalize lg:!leading-[80px] text-primary">
+        <div className="order-2 flex flex-col items-center lg:items-start gap-4 lg:order-1">
+          <div className="hidden lg:flex">
+            <PageNotFoundPill />
+          </div>{" "}
+          <div className="flex flex-col gap-4 lg:gap-20 items-center lg:items-start">
+            <div className="flex flex-col gap-1 items-center lg:items-start">
+              <h1 className="text-center capitalize text-primary lg:text-left lg:!leading-[80px]">
                 <span className="text-accent">
                   404. <br />
                 </span>
-                This page does <br /> not exist.
+                This page does <br className="hidden lg:flex" /> not exist.``
               </h1>
             </div>
 
-            <Link href={"/"} className="">
+            <Link href={"/"}>
               <Button
                 variant={"outline"}
-                className="h-20 w-full max-w-[288px] p-4"
+                className="h-10 w-fit lg:w-[288px] p-4 lg:h-20 "
               >
                 Return Home
               </Button>
             </Link>
           </div>
         </div>
-        <div
-          className="relative aspect-[709/540] w-full max-w-[709px]"
-          style={{ animationDuration: "2s" }}
-        >
-          <Image src={"/assets/images/404-error2.svg"} fill alt="404 Error" />
+        <div className="flex  order-1 lg:order-2 w-full flex-col gap-2 items-center max-w-[709px]">
+          <div className="lg:hidden">
+            <PageNotFoundPill />
+          </div>{" "}
+          <div
+            className="relative  aspect-[709/540] w-full max-w-[709px] "
+            style={{ animationDuration: "2s" }}
+          >
+            <Image src={"/assets/images/404-error2.svg"} fill alt="404 Error" />
+          </div>
         </div>
       </div>
-      <div className={`w-full p-20 ${styles.container}`}>
+      <div className={`w-full py-20 ${styles.container}`}>
         <PageNotFoundDiscover />
       </div>{" "}
     </div>
